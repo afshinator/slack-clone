6e3511c6aa363349ef7297dd01f3c3d4e43de89c
Afshin Mokhtari <afshinator@gmail.com>
2025-08-27 15:43:17 -0700

    add test server response

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+
+const app = express();
+
+app.get('/', (req,res) => {
+  res.send("hello world")
+})
+
+app.listen(5001, () => {
+  console.log("Server started on port:", 5001);
+});
