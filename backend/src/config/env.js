a5319148bd8c66e3de3d775ef337a48e408aa21a
Afshin Mokhtari <afshinator@gmail.com>
2025-09-09 15:44:04 -0700

    backedn: add stream config

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -6,7 +6,9 @@ export const ENV = {
   NODE_ENV: process.env.NODE_ENV,
   CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
   CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-  
+  STREAM_API_KEY: process.env.STREAM_API_KEY,
+  STREAM_API_SECRET: process.env.STREAM_API_SECRET,
+
 };
 
 /*
